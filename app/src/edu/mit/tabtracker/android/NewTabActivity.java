a59ba0c604ed06005386fdedf28edfb40555eb67
Mark Yen <markyen@mit.edu>
2011-07-30 14:18:25 -0700

    Last changes. still doesnt work. pos.

--- app/src/edu/mit/tabtracker/android/NewTabActivity.java
+++ app/src/edu/mit/tabtracker/android/NewTabActivity.java
@@ -3,9 +3,13 @@
  */
 package edu.mit.tabtracker.android;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -31,11 +35,12 @@ public class NewTabActivity extends Activity {
     private ListView mList;
     private int mEditLocation;
     private ArrayAdapter<Item> mListAdapter;
+    BigDecimal mTotal = new BigDecimal(0);
     
     private class ItemAdapter extends ArrayAdapter<Item>{
-        private Item[] items;
+        private List<Item> items;
 
-        public ItemAdapter(Item[] items) {
+        public ItemAdapter(List<Item> items) {
             super(NewTabActivity.this, R.layout.new_tab, items);
             this.items = items;
         }
@@ -49,41 +54,34 @@ public class NewTabActivity extends Activity {
         public View getView(int position, View convertView, ViewGroup parent) {
             final ViewHolder holder;
             View rowView = convertView;
-            final Item item = items[position];
-            if (rowView == null) {
-                LayoutInflater inflater = NewTabActivity.this.getLayoutInflater();
-                if (item.editable) {
-                    rowView = inflater.inflate(R.layout.edit_item, null, true);
-                } else {
-                    rowView = inflater.inflate(R.layout.view_item, null, true);
+            final Item item = items.get(position);
+            if (!item.total) {
+                if (rowView == null) {
+                    LayoutInflater inflater = mLayoutInflater;
+                    if (item.editable) {
+                        rowView = inflater.inflate(R.layout.edit_item, null, true);
+                    } else {
+                        rowView = inflater.inflate(R.layout.view_item, null, true);
+                    }
+                    holder = new ViewHolder();
+                    holder.payerView = (TextView) rowView.findViewById(R.id.payer);
+                    holder.costView = (TextView) rowView.findViewById(R.id.cost);
+                    rowView.setTag(holder);
+                    holder.payerView.setText(item.payer);
+                    holder.costView.setText(item.cost);
+                } else if (rowView.getTag() != null) {
+                    holder = (ViewHolder) rowView.getTag();
+                    holder.payerView.setText(item.payer);
+                    holder.costView.setText(item.cost);
                 }
-                holder = new ViewHolder();
-                holder.payerView = (TextView) rowView.findViewById(R.id.payer);
-                holder.costView = (TextView) rowView.findViewById(R.id.cost);
-                rowView.setTag(holder);
             } else {
-                holder = (ViewHolder) rowView.getTag();
+                rowView = mLayoutInflater.inflate(R.layout.view_item, null, true);
+                rowView.setOnClickListener(null);
+                ((TextView) rowView.findViewById(R.id.payer)).setText("TOTAL:");
+                ((TextView) rowView.findViewById(R.id.cost)).setText(mTotal.toPlainString());
             }
-
-            holder.payerView.setText(item.payer);
-            holder.costView.setText(item.cost);
-
             return rowView;
             
-            /*
-            final Item item = getItem(position);
-            if (item.editable) {
-                final View edit = mLayoutInflater.inflate(R.layout.edit_item, null);
-                ((EditText) edit.findViewById(R.id.payer)).setText(item.payer);
-                ((EditText) edit.findViewById(R.id.cost)).setText(item.cost);
-                return edit;
-            } else {
-                final View view = mLayoutInflater.inflate(R.layout.view_item, null);
-                ((TextView) view.findViewById(R.id.payer)).setText(item.payer);
-                ((TextView) view.findViewById(R.id.cost)).setText(item.cost);
-                return view;
-            }
-            //*/
         }
     }
 
@@ -94,9 +92,9 @@ public class NewTabActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_tab);
-        mLayoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mLayoutInflater = (LayoutInflater) getLayoutInflater();
         mEditLocation = 0;
-        mListAdapter = new ItemAdapter(new Item[] { new Item() });
+        mListAdapter = new ItemAdapter(new ArrayList<Item>(Arrays.asList(new Item[] { new Item(), new Item(true) })));
         final ListView list = (ListView)findViewById(R.id.list);
         list.setAdapter(mListAdapter);
         list.setOnItemClickListener(new OnItemClickListener() {
@@ -111,7 +109,6 @@ public class NewTabActivity extends Activity {
                 mListAdapter.notifyDataSetChanged();
             }
         });
-//        setListAdapter(mListAdapter);
     }
 
     public void pickContactHandler(View target) {
@@ -126,8 +123,8 @@ public class NewTabActivity extends Activity {
 
     public void newItemHandler(View target) {
         mListAdapter.getItem(mEditLocation).editable = false;
-        mListAdapter.add(new Item());
-        mEditLocation = mListAdapter.getCount();
+        mListAdapter.insert(new Item(), mEditLocation);
+        mEditLocation = mListAdapter.getCount()-2;
         /*
         saveCurrentEdit();
         final TableRow newRow = (TableRow) mLayoutInflater.inflate(R.layout.edit_item, null);
@@ -136,33 +133,33 @@ public class NewTabActivity extends Activity {
         //*/
     }
 
-    public void editItemHandler(View target) {
-        saveCurrentEdit();
-        mEditLocation = mList.indexOfChild(target);
-        final CharSequence payer = ((TextView) target.findViewById(R.id.payer)).getText();
-        final CharSequence cost = ((TextView) target.findViewById(R.id.cost)).getText();
-        final TableRow edit = (TableRow) mLayoutInflater.inflate(R.layout.edit_item, null);
-        ((EditText) edit.findViewById(R.id.payer)).setText(payer);
-        ((EditText) edit.findViewById(R.id.cost)).setText(cost);
-        mList.removeViewAt(mEditLocation);
-        mList.addView(edit, mEditLocation);
-    }
+//    public void editItemHandler(View target) {
+//        saveCurrentEdit();
+//        mEditLocation = mList.indexOfChild(target);
+//        final CharSequence payer = ((TextView) target.findViewById(R.id.payer)).getText();
+//        final CharSequence cost = ((TextView) target.findViewById(R.id.cost)).getText();
+//        final TableRow edit = (TableRow) mLayoutInflater.inflate(R.layout.edit_item, null);
+//        ((EditText) edit.findViewById(R.id.payer)).setText(payer);
+//        ((EditText) edit.findViewById(R.id.cost)).setText(cost);
+//        mList.removeViewAt(mEditLocation);
+//        mList.addView(edit, mEditLocation);
+//    }
 
-    private void saveCurrentEdit() {
-        final View edit = mList.getChildAt(mEditLocation);
-        final CharSequence payer = ((EditText) edit.findViewById(R.id.payer)).getText();
-        final CharSequence cost = ((EditText) edit.findViewById(R.id.cost)).getText();
-        if (!(TextUtils.isEmpty(payer) && TextUtils.isEmpty(cost))) {
-            final TableRow view = (TableRow) mLayoutInflater.inflate(R.layout.view_item, null);
-            ((TextView) view.findViewById(R.id.payer)).setText(payer);
-            ((TextView) view.findViewById(R.id.cost)).setText(cost);
-            final int index = mList.indexOfChild(edit);
-            mList.removeViewAt(index);
-            mList.addView(view, index);
-        } else {
-            mList.removeView(edit);
-        }
-    }
+//    private void saveCurrentEdit() {
+//        final View edit = mList.getChildAt(mEditLocation);
+//        final CharSequence payer = ((EditText) edit.findViewById(R.id.payer)).getText();
+//        final CharSequence cost = ((EditText) edit.findViewById(R.id.cost)).getText();
+//        if (!(TextUtils.isEmpty(payer) && TextUtils.isEmpty(cost))) {
+//            final TableRow view = (TableRow) mLayoutInflater.inflate(R.layout.view_item, null);
+//            ((TextView) view.findViewById(R.id.payer)).setText(payer);
+//            ((TextView) view.findViewById(R.id.cost)).setText(cost);
+//            final int index = mList.indexOfChild(edit);
+//            mList.removeViewAt(index);
+//            mList.addView(view, index);
+//        } else {
+//            mList.removeView(edit);
+//        }
+//    }
 
     /* (non-Javadoc)
      * @see android.app.ListActivity#onListItemClick(android.widget.ListView, android.view.View, int, long)
@@ -183,9 +180,10 @@ public class NewTabActivity extends Activity {
         CharSequence payer = "";
         CharSequence cost = "";
         boolean editable = true;
+        boolean total = false;
         public Item() { }
-        @Override public String toString() {
-            return payer.toString() + ": " + cost.toString();
+        public Item(boolean total) {
+            this.total = total;
         }
     }
 }
