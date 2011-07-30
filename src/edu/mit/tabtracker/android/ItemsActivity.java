4124e81f1b5d1175621504ce2c510b81bb7bfb47
Mark Yen <markyen@mit.edu>
2011-07-29 22:18:44 -0700

    Initial Android project

---
+++ src/edu/mit/tabtracker/android/ItemsActivity.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package edu.mit.tabtracker.android;
+
+import android.app.Activity;
+
+/**
+ * @author
+ *
+ */
+public class ItemsActivity extends Activity {
+
+}
---
+++ src/edu/mit/tabtracker/android/NewItemActivity.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package edu.mit.tabtracker.android;
+
+import android.app.Activity;
+
+/**
+ * @author
+ *
+ */
+public class NewItemActivity extends Activity {
+
+}
