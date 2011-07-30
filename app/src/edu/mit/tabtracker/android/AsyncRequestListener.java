b6aa702e39d11494fc4eade3a602111f49c7f3d9
Jeff Chan <jefftchan@gmail.com>
2011-07-30 03:08:36 -0700

    new files

---
+++ app/src/edu/mit/tabtracker/android/AsyncRequestListener.java
@@ -0,0 +1,51 @@
+package edu.mit.tabtracker.android;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+import com.facebook.android.FacebookError;
+import com.facebook.android.Util;
+import com.facebook.android.AsyncFacebookRunner.RequestListener;
+
+abstract class AsyncRequestListener implements RequestListener {
+
+    public void onComplete(String response, final Object state) {
+        try {
+            JSONObject obj = Util.parseJson(response);
+            onComplete(obj, state);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.e("facebook-stream", "JSON Error:" + e.getMessage());
+        } catch (FacebookError e) {
+            Log.e("facebook-stream", "Facebook Error:" + e.getMessage());
+        }
+
+    }
+
+    public abstract void onComplete(JSONObject obj, final Object state);
+
+    public void onFacebookError(FacebookError e, final Object state) {
+        Log.e("stream", "Facebook Error:" + e.getMessage());
+    }
+
+    public void onFileNotFoundException(FileNotFoundException e,
+                                        final Object state) {
+        Log.e("stream", "Resource not found:" + e.getMessage());      
+    }
+
+    public void onIOException(IOException e, final Object state) {
+        Log.e("stream", "Network Error:" + e.getMessage());      
+    }
+
+    public void onMalformedURLException(MalformedURLException e,
+                                        final Object state) {
+        Log.e("stream", "Invalid URL:" + e.getMessage());            
+    }
+
+}
---
+++ app/src/edu/mit/tabtracker/android/FriendsListSingleton.java
@@ -0,0 +1,29 @@
+package edu.mit.tabtracker.android;
+
+public class FriendsListSingleton {
+	private static final FriendsListSingleton instance = new FriendsListSingleton();
+	private Integer[] friendIds;
+	private String[] friendNames;
+	 
+    // Private constructor prevents instantiation from other classes
+    private FriendsListSingleton() {
+    	
+    }
+ 
+    public static FriendsListSingleton getInstance() {
+        return instance;
+    }
+    
+    public void store(Integer[] ids, String[] names) {
+    	friendIds = ids;
+    	friendNames = names;
+    }
+    
+    public Integer[] getFriendIds() {
+    	return friendIds;
+    }
+    
+    public String[] getFriendNames() {
+    	return friendNames;
+    }
+}
