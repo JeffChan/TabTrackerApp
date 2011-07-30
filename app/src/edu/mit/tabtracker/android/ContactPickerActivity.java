be6ca140adfe28fbc770cd057697ba9393432ee4
Jeff Chan <jefftchan@gmail.com>
2011-07-29 23:45:34 -0700

    Moved folders around

---
+++ app/src/edu/mit/tabtracker/android/ContactPickerActivity.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.mit.tabtracker.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author
+ *
+ */
+public class ContactPickerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+//		Intent intent = new Intent(this, );
+//		super.onCreate(savedInstanceState);
+	}
+	
+}
